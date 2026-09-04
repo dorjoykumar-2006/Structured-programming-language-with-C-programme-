c6c316286bf30f0da2fd61e384986e217dd6e5ea
Dorjoykumar-2006 <dorjoy.kumar2006@gmail.com>
2026-09-04 14:15:39 +0600

    Updated C program

--- Demo.c
+++ Demo.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-#include <stdbool.h>
 int main()
 {
-    int x = 20;
-int y = 18;
+printf("hello world");
 
-bool isGreater = x > y;
 
-if (isGreater) {
-  printf("x is greater than y");
-}
-    
+
     return 0;
 }
