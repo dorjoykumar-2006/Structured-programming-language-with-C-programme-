dabb42e55eddf6156e156587da287bcc096841ad
Dorjoykumar-2006 <dorjoy.kumar2006@gmail.com>
2026-09-04 20:33:46 +0600

    intial commit

---
+++ C_TypeConversions.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+
+    int a = 5; 
+    int b = 2; 
+
+    int sum = a/b;
+    printf("Sum is: %d\n", sum);
+    return 0;
+}
---
+++ C_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int age=18;
+    if(age>=18)
+    {
+        printf("Allowed\n");
+    }
+    else
+    {
+        printf("Not Allowed\n");
+    }
+    return 0;
+}
